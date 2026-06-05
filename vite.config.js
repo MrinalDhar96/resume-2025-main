d75c37998403d3cec2018df52cee5b6667b26a3f
Mrinal Dhar <mrinal.dhar10@gmail.com>
2026-06-05 21:31:23 +0530

    docs update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
+  base: '/resume-2025-main/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['react-vertical-timeline-component']
+  },
+  build: {
+    outDir: 'docs', 
   }
 })
-
-
